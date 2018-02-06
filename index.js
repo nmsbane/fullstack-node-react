1528f85a0d36bf385748afeca1f3c9c322cfadc6
Madhusudan <madhusudhan638@gmail.com>
2018-02-06 14:07:33 +0000

    Installed passport and google oauth strategy. Learnt the basics of how the oAuth works.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+const passport = require("passport");
+// we are intrested in getting a Strategy property of passport-google-oauth20
+const GoogleOAuthStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(new GoogleOAuthStrategy());
 
 const PORT = process.env.PORT || 8080;
 
