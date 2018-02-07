4cb9f1922ed266185abb3a7dd7e6fb3ae15ff3b9
Madhusudan <madhusudhan638@gmail.com>
2018-02-07 14:19:28 +0000

    Learnt about the importance of redirct_uri when configuring oAuth, and why is it important for security reasons.

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
-
 const passport = require("passport");
 // we are intrested in getting a Strategy property of passport-google-oauth20
 const GoogleOAuthStrategy = require("passport-google-oauth20").Strategy;
+// get the google oauth keys
+const keys = require("./config/keys");
 
-// client ID
 
-// client secret
-passport.use(new GoogleOAuthStrategy());
+passport.use(new GoogleOAuthStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+    console.log(accessToken);
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
 
 const PORT = process.env.PORT || 8080;
 
