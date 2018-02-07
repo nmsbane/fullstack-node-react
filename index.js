5501a49d6e0e107865141aac2d78b8c2c11bf406
Madhusudan <madhusudhan638@gmail.com>
2018-02-07 13:31:16 +0000

    create googel authentication keys and store them securely in the project.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const passport = require("passport");
 // we are intrested in getting a Strategy property of passport-google-oauth20
 const GoogleOAuthStrategy = require("passport-google-oauth20").Strategy;
 
+// client ID
+
+// client secret
 passport.use(new GoogleOAuthStrategy());
 
 const PORT = process.env.PORT || 8080;
