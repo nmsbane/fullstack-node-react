39e371ca74ceaa2adeb4bc13a96b6fe8a7924111
Madhusudan <madhusudhan638@gmail.com>
2018-02-08 14:23:11 +0000

    completed the OAuth flow, and able to get the user profile after exchanging the code with google. Learnt about nodemon and as well as, "scripts" property inside the package.json file

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ const keys = require("./config/keys");
 passport.use(new GoogleOAuthStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken) => {
+    callbackURL: "https://" + process.env.C9_HOSTNAME + "/auth/google/callback"
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(refreshToken);
     console.log(accessToken);
+    console.log(profile);
 }));
 
 app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
 }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 
 const PORT = process.env.PORT || 8080;
 
