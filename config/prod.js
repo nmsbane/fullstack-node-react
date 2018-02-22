05f2f3bc136c31edfe0626a6bbcdf2d8079e4421
Madhusudan <madhusudhan638@gmail.com>
2018-02-22 16:46:21 +0000

    Add environment variables for heroku, and finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// production keys here --
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongodbURI: process.env.MONGODB_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
